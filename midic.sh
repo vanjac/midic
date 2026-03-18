0313a46a698f3e637eb9cf95395d8b968321ee23
j <chroma@chroma.zone>
2026-03-17 22:18:07 -0700

    comment

--- midic.sh
+++ midic.sh
@@ -9,7 +9,7 @@ case "${1:-}" in
 esac
 
 rm -f "$1"
-# Skip comments ('#') and lines between < >
+# Skip comments ('#') and lines between < > markers
 sed 's/#.*//;/^</,/^>/{/^>/!d};s/^>//' | xxd -r -p - "$1"
 # Calculate and overwrite MTrk chunk size
 printf "%08x" $(($(stat -c "%s" "$1") - 22)) | xxd -r -p -s 18 - "$1"
