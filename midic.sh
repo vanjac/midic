1ff6ee77a60198a078f9568941c904f1344e18cd
j <chroma@chroma.zone>
2026-03-17 22:16:29 -0700

    Allow angle brackets to skip lines

--- midic.sh
+++ midic.sh
@@ -9,7 +9,7 @@ case "${1:-}" in
 esac
 
 rm -f "$1"
-# Skip comments ('#') and all unrecognized characters
-sed 's/#.*//' | xxd -r -p - "$1"
+# Skip comments ('#') and lines between < >
+sed 's/#.*//;/^</,/^>/{/^>/!d};s/^>//' | xxd -r -p - "$1"
 # Calculate and overwrite MTrk chunk size
 printf "%08x" $(($(stat -c "%s" "$1") - 22)) | xxd -r -p -s 18 - "$1"
