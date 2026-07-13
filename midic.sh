ca1541abee206339a0af37dabbfbe6b3e924ca56
j <chroma@chroma.zone>
2026-07-13 08:03:23 -0700

    Replace sed and seq with awk

--- midic.sh
+++ midic.sh
@@ -9,16 +9,19 @@ case "${1:-}" in
 esac
 
 rm -f "$1"
-sed 's/#.*//;/^</,/^>/{/^>/!d};s/^>//' | # Skip comments ('#') and lines between < > markers
-	while read -r line; do
-		case "$line" in
-			*[*]*) # Match loop syntax
-				printf "%02x\n" $(seq ${line##*[*]}) | while read hex; do
-					printf "%s" "${line%[*]*}" | sed "s/__/$hex/g"
-				done ;;
-			*) printf "%s" "$line" ;;
-		esac
-	done |
-	xxd -r -p - "$1"
+awk -Wposix '
+	{ sub(/#.*/, "") }
+	/^</,/^>/ { if (/^>/) sub(/^>/, ""); else next }
+	/\*/ {
+		split($0, parts, "*")
+		for (i = 0; i < parts[2]; i++) {
+			v = i / parts[2] * parts[4] + (parts[2] - i) / parts[2] * parts[3]
+			line = parts[1]; gsub(/__/, sprintf("%02x", v), line)
+			print line
+		}
+		next
+	}
+	{ print }
+' | xxd -r -p - "$1"
 # Calculate and overwrite MTrk chunk size
 printf "%08x" $(($(stat -c "%s" "$1") - 22)) | xxd -r -p -s 18 - "$1"
