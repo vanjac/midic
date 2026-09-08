a556ed35b829285bb12dedfaff87bf30ed1ab425
j <chroma@chroma.zone>
2026-09-07 20:16:07 -0700

    Split midi events into separate lines

--- midic.py
+++ midic.py
@@ -17,21 +17,25 @@ parser.add_argument('-o', '--output', default='-', type=argparse.FileType('wb'))
 parser.add_argument('-f', '--format', default='hex', choices=['hex', 'smf'])
 args = parser.parse_args()
 
-def parsefields(ev, count='1', start='0', end='0'):
-    return ev, int(count, 0), int(start, 0), int(end, 0)
+def parsefields(hexstr, count='1', start='0', end='0'):
+    return hexstr, int(count, 0), int(start, 0), int(end, 0)
 skip = False
 for line in args.input:
     skip = (skip or line.startswith('<')) and not line.startswith('>')
     if not skip:
         f = re.sub(r'(^>|#.*|[*-/])', '', line).split(':')
-        ev, count, start, end = parsefields(*f)
+        hexstr, count, start, end = parsefields(*f)
         for i in range(count):
             val = int(i / count * end + (count-i) / count * start)
-            b = bytes.fromhex(ev.replace('__', f'{val:02x}'))
+            b = bytes.fromhex(hexstr.replace('__', f'{val:02x}'))
             if args.format == 'smf':
                 args.output.write(b)
-            elif args.format == 'hex':
-                args.output.write((b.hex() + '\n').encode())
+            else:
+                cmdidx = 0
+                for i in range(1, len(b) + 1):
+                    if i == len(b) or (b[i] & 0x80) != 0:
+                        args.output.write((b[cmdidx:i].hex() + '\n').encode())
+                        cmdidx = i
 if args.format == 'smf':
     size = args.output.tell() - 22
     args.output.seek(18, os.SEEK_SET)
